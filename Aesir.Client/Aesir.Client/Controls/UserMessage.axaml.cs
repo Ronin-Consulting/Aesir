a835ec3b794784f68c2435d6a9807af6bf6b6ca7
Jeffery Summers <jefferysummers@Mac.attlocal.net>
2025-06-12 12:55:44 -0500

    [WIP] :: user message action panel with edit button on hover

--- Aesir.Client/Aesir.Client/Controls/UserMessage.axaml.cs
+++ Aesir.Client/Aesir.Client/Controls/UserMessage.axaml.cs
@@ -1,4 +1,7 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace Aesir.Client.Controls;
 
@@ -7,5 +10,32 @@ public partial class UserMessage : UserControl
     public UserMessage()
     {
         InitializeComponent();
+        
+        PointerEntered += OnPointerEntered;
+        PointerExited += OnPointerExited;
+        PointerCaptureLost += OnPointerCaptureLost;
+    }
+
+    private void OnPointerEntered(object? sender, PointerEventArgs e)
+    {
+        if (ActionPanel == null) return;
+        ActionPanel.Opacity = 1.0;
+    }
+
+    private void OnPointerExited(object? sender, PointerEventArgs e)
+    {
+        if (ActionPanel == null) return;
+        ActionPanel.Opacity = 0.0;
+    }
+
+    private void OnPointerCaptureLost(object? sender, PointerCaptureLostEventArgs e)
+    {
+        if (ActionPanel == null) return;
+        ActionPanel.Opacity = 0.0;
+    }
+
+    public void EditAction_Clicked(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("EDIT ACTION CLICKED!");
     }
 }
