f76882aab634e2eb8aa3068879103de0e4341f63
ooartist <byron.mcclain@ronin.consulting>
2025-07-06 20:34:55 -0500

    Add XML documentation to database context, extensions, and type handler
    Introduced detailed XML documentation for `PgDbContext`, `IDbContext`, `DbContextExtensions`, and `JsonTypeHandler`. Included comprehensive summaries, parameter descriptions, and functionality details to enhance codebase readability and maintenance.

--- Aesir.Api.Server/Data/JsonTypeHandler.cs
+++ Aesir.Api.Server/Data/JsonTypeHandler.cs
@@ -4,13 +4,28 @@ using Newtonsoft.Json;
 
 namespace Aesir.Api.Server.Data;
 
+/// <summary>
+/// A custom Dapper TypeHandler that handles serialization and deserialization
+/// of objects to and from JSON format in database columns.
+/// </summary>
+/// <typeparam name="T">The type of the object that this handler will serialize and deserialize.</typeparam>
 public class JsonTypeHandler<T> : SqlMapper.TypeHandler<T>
 {
+    /// <summary>
+    /// Sets the value of the parameter by serializing the provided object to a JSON string.
+    /// </summary>
+    /// <param name="parameter">The database parameter that will be assigned the JSON string value.</param>
+    /// <param name="value">The object to serialize into a JSON string to set as the parameter value.</param>
     public override void SetValue(IDbDataParameter parameter, T? value)
     {
         parameter.Value = JsonConvert.SerializeObject(value);
     }
 
+    /// <summary>
+    /// Deserializes a JSON string into an object of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="value">The JSON string to be deserialized.</param>
+    /// <returns>An object of type <typeparamref name="T"/> deserialized from the provided JSON string.</returns>
     public override T Parse(object value)
     {
         return JsonConvert.DeserializeObject<T>((string)value)!;
--- Aesir.Api.Server/Data/PgDbContext.cs
+++ Aesir.Api.Server/Data/PgDbContext.cs
@@ -3,8 +3,16 @@ using Npgsql;
 
 namespace Aesir.Api.Server.Data;
 
+/// <summary>
+/// Represents a database context for PostgreSQL connections. Implements the <see cref="IDbContext"/> interface
+/// and provides functionality to establish and manage database connections.
+/// </summary>
 public class PgDbContext(string connectionString) : IDbContext
 {
+    /// <summary>
+    /// Establishes and returns a new database connection using the configured connection string.
+    /// </summary>
+    /// <returns>A new instance of an <see cref="IDbConnection"/> for interacting with the database.</returns>
     public IDbConnection GetConnection()
     {
         return new NpgsqlConnection(connectionString);
